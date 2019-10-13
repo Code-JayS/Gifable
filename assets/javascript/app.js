98eba84834046d0fe2a7069bedc039e430fae960
J. Smith <joe.a.smith@gmail.com>
2019-10-12 23:48:35 -0500

    forgot to save pushing mobile first version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,11 +52,8 @@ function renderButtons() {
 $(document.body).on("click", "#gifControl", gifStateChange);
 
 function gifStateChange() {
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    // get and set the value of data-states 
     var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
     if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
       $(this).attr("data-state", "animate");
